8ebb9b0eeff172ac292250fabd5478a6d46bba94
Anton Thunberg <anton@thunberg.nu>
2021-12-08 17:58:34 +0100

    last one

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-console.log("hej")
 
 //arry to fil with movies
 let starMovies = [];
@@ -6,45 +5,66 @@ let starMovies = [];
 //fetch all the movies
 //maps out all the movies
 //and set the title and release date
-//"index" is to give every "star-cards" div an index
+//"index" gives every "star-cards" div an index
 const starFetch = async () => {
     try {
+        //fetch the movies
         const res = await fetch(`https://swapi.dev/api/films`);
         const data = await res.json();
-        starMovies = data;
-        console.log(starMovies)
-        document.querySelector("#main-wrapper").innerHTML = starMovies.results.map((star, index) => {
+        //adds the results to the starMovies array
+        starMovies = data.results;
+        //maps out all the movies with title and release date, and gives an index
+        document.querySelector("#main-wrapper").innerHTML = starMovies.map((star, index) => {
             return `<div class="star-cards" onclick="openModal(${index})"><h3>${star.title}</h3></br><p>${star.release_date}</p></div>`
         }).join("");
+        
+    
     }catch(error){
-        alert(error)
+        //logs error is it fails
+        console.log(error)
     }; 
 };
 
+//characters comes in url format
+//this fetches all the urls
 const starChar = (index) => {
-    const people = starMovies.results[index].characters.map((url) => {
-        fetch(url).then((res) => res.json())
-    
-       
+    const people = starMovies[index].characters.map((url) => {
+        return fetch(url).then(res => res.json())
     });
-    
     return people;
-    
-};
-//console.log(people)
-
-
-const openModal = (index) => {
-    Promise.all(starChar(index))
-    //.then((res) => res.json());
-    document.getElementById("modal-wrapper").style.display = "flex";
-    document.getElementById("star-header").innerHTML = `<h2>${starMovies.results[index].title}</h2>`
-    //document.getElementById("star-content").innerHTML = `<p>${starChar(index)}</p>`
-    //console.log(starChar(index))
-    //console.log(people)
-    
 };
 
+//when you click a modal this function runs
+//with the given index to get the right movie
+const openModal = async (index) => {
+   
+   //when you click a modal, it sets the div to display flex to be shown
+   document.getElementById("modal-wrapper").style.display = "flex";
+   
+    //this runs the fetch for all the urls
+    const poepleArray = starChar(index)
+    const charactersArray = await Promise.all(poepleArray).catch((err) => alert(err))
+    console.log(charactersArray)
+
+    //renders out the movie title of the card you clicked on
+    document.getElementById("star-header").innerHTML = `<h2>${starMovies[index].title}</h2> 
+    <span><i class="fa fa-window-close" aria-hidden="true" onclick="closeStar()"></i></span>`
+    //this maps out all the names of the characters in that movie
+    document.querySelector("#star-content").innerHTML = charactersArray.map((test) => {
+        return `<p>${test.name}</p>`
+        //then sorts by first letter and removes the ","
+    }).sort().join("")
+    
+};
+
+//when you press the x on the card it changes to display none to be hidden
+const closeStar = () => {
+    document.getElementById("modal-wrapper").style.display = "none";
+    document.getElementById("star-content").innerHTML = `<div class="star-loader-content"></div>`
+    document.getElementById("star-header").innerHTML = `<h2>Loading</h2>`
+}
+
+//runs starFetchs on load to map out the movies
 window.addEventListener("load", () => {
     starFetch();
 })
